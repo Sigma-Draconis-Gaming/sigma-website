0f2e282aeda317b2866590b4040da5d1b5b626cb
Tails <hydrabott@gmail.com>
2020-03-04 15:19:35 -0500

    static

---
+++ static/assets/js/application.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+
+    const socket = io.connect(`https://api.sigmadraconis.games/wss`);
+
+    const get_status = (data) => {
+        if (data.status === "Down"){
+            return 'negative'
+        }
+        else if (data.players === data.max_players){
+            return 'active'
+        }
+        else if (data.ping >= 200.00){
+            return 'intermediary'
+        }
+        else if (data.status === "ok"){
+            return 'positive'
+        }
+        else {
+            return 'negative'
+        }
+
+    };
+
+    socket.on('server_update', msg => {
+        let {server} = msg;
+        console.log(server.name);
+        $(`#${server.name.replace(/\s+/g, '')}`).html(`<b>${server.name}: ${server.ip} </b><br><b><status-indicator ${get_status(server)} pulse></status-indicator> Status:  </b> <b>  Ping</b>: ${server.ping} <a href="steam://connect/${server.ip}" class="button primary small fit icon solid fa-user">${server.players}/${server.max_players} Join!</a>`)
+    });
+});
